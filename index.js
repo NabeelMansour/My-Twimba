11954ce91bffdb77be2a069f2d7d3fa7bdd0a337
Nabeel Mansour <59266812+NabeelMansour@users.noreply.github.com>
2024-03-06 15:57:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,116 +1,103 @@
-import { tweetsData } from './data.js'
-import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+import { tweetsData } from "./data.js";
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.like) {
+    handleLikeClick(e.target.dataset.like);
+  } else if (e.target.dataset.retweet) {
+    handleRetweetClick(e.target.dataset.retweet);
+  } else if (e.target.dataset.reply) {
+    handleReplyClick(e.target.dataset.reply);
+    handleReplies(e.target.dataset.reply);
+  } else if (e.target.id === "tweet-btn") {
+    handleTweetBtnClick();
+    render();
+  } else if (e.target.id === "close") {
+    // e.target.id.classList.add('hidden')
+    closeTweet();
+  }
+});
 
-document.addEventListener('click', function(e){
-    if(e.target.dataset.like){
-       handleLikeClick(e.target.dataset.like) 
-    }
-    else if(e.target.dataset.retweet){
-        handleRetweetClick(e.target.dataset.retweet)
-    }
-    else if(e.target.dataset.reply){
-        handleReplyClick(e.target.dataset.reply)
-        handleReplies(e.target.dataset.reply)
-    }
-    else if(e.target.id === 'tweet-btn'){
-        handleTweetBtnClick()
-        render()
-    }
-    else if(e.target.id === 'close'){
-        // e.target.id.classList.add('hidden')
-        closeTweet()
-    }
-})
+function handleLikeClick(tweetId) {
+  const targetTweetObj = tweetsData.filter(function (tweet) {
+    return tweet.uuid === tweetId;
+  })[0];
 
- 
-function handleLikeClick(tweetId){ 
-    const targetTweetObj = tweetsData.filter(function(tweet){
-        return tweet.uuid === tweetId
-    })[0]
-
-    if (targetTweetObj.isLiked){
-        targetTweetObj.likes--
-    }
-    else{
-        targetTweetObj.likes++ 
-    }
-    targetTweetObj.isLiked = !targetTweetObj.isLiked
-    render()
+  if (targetTweetObj.isLiked) {
+    targetTweetObj.likes--;
+  } else {
+    targetTweetObj.likes++;
+  }
+  targetTweetObj.isLiked = !targetTweetObj.isLiked;
+  render();
 }
 
+function handleRetweetClick(tweetId) {
+  const targetTweetObj = tweetsData.filter(function (tweet) {
+    return tweet.uuid === tweetId;
+  })[0];
 
-function handleRetweetClick(tweetId){
-    const targetTweetObj = tweetsData.filter(function(tweet){
-        return tweet.uuid === tweetId
-    })[0]
-    
-    if(targetTweetObj.isRetweeted){
-        targetTweetObj.retweets--
-    }
-    else{
-        targetTweetObj.retweets++
-    }
-    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
-    
-    render() 
+  if (targetTweetObj.isRetweeted) {
+    targetTweetObj.retweets--;
+  } else {
+    targetTweetObj.retweets++;
+  }
+  targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted;
+
+  render();
 }
 
-function handleReplyClick(replyId){
-    document.getElementById(`replies-${replyId}`).classList.toggle('hidden')   
+function handleReplyClick(replyId) {
+  document.getElementById(`replies-${replyId}`).classList.toggle("hidden");
 }
 
-function handleTweetBtnClick(){
-    const tweetInput = document.getElementById('tweet-input')
-    
-    if(tweetInput.value){
-        tweetsData.unshift({
-            handle: `@Scrimba`,
-            profilePic: `images/scrimbalogo.png`,
-            likes: 0,
-            retweets: 0,
-            tweetText: tweetInput.value,
-            replies: [],
-            isLiked: false,
-            isRetweeted: false,
-            uuid: uuidv4()
-        })
-    render()
-    tweetInput.value = ''
+function handleTweetBtnClick() {
+  const tweetInput = document.getElementById("tweet-input");
+
+  if (tweetInput.value) {
+    tweetsData.unshift({
+      handle: `@Scrimba`,
+      profilePic: `images/scrimbalogo.png`,
+      likes: 0,
+      retweets: 0,
+      tweetText: tweetInput.value,
+      replies: [],
+      isLiked: false,
+      isRetweeted: false,
+      uuid: uuidv4(),
+    });
+    render();
+    tweetInput.value = "";
+  }
+}
+
+function getFeedHtml() {
+  let feedHtml = ``;
+
+  tweetsData.forEach(function (tweet) {
+    let likeIconClass = "";
+
+    if (tweet.isLiked) {
+      likeIconClass = "liked";
     }
-}
 
+    let retweetIconClass = "";
 
+    if (tweet.isRetweeted) {
+      retweetIconClass = "retweeted";
+    }
 
-function getFeedHtml(){
-    let feedHtml = ``
-    
-
-    tweetsData.forEach(function(tweet){
-        
-        let likeIconClass = ''
-        
-        if (tweet.isLiked){
-            likeIconClass = 'liked'
-        }
-        
-        let retweetIconClass = ''
-        
-        if (tweet.isRetweeted){
-            retweetIconClass = 'retweeted'
-        }
-        
-            let repliesHtml = `  
+    let repliesHtml = `  
                 <div class="tweet-input-area">
                     <img src="images/Nabil.jpg" class="profile-pic">
                     <textarea placeholder="Your reply" id="${tweet.uuid}"></textarea>
                 </div>
                 <button id="reply-btn-${tweet.uuid}">Reply</button>
-                `
-                        
-                if(tweet.replies.length > 0){
-                    tweet.replies.forEach(function(reply){
-                        repliesHtml += `
+                `;
+
+    if (tweet.replies.length > 0) {
+      tweet.replies.forEach(function (reply) {
+        repliesHtml += `
                         
                     <div class="tweet-reply">
                         <div class="tweet-inner">
@@ -121,19 +108,18 @@ function getFeedHtml(){
                             </div>
                         </div>
                     </div>
-                `
-            })
-        }
-              
-          
-        feedHtml += `
+                `;
+      });
+    }
+
+    feedHtml += `
 <div class="tweet" id="tweet">
     <span class="close">
         <i class="fa-solid fa-xmark" id="close"></i>
     </span>
     <div class="tweet-inner" >
     
-        <img src="${tweet.profilePic}" class="profile-pic">
+        <img src="./images/${tweet.profilePic}" class="profile-pic">
         <div>
             <p class="handle">${tweet.handle}</p>
             <p class="tweet-text">${tweet.tweetText}</p>
@@ -164,51 +150,54 @@ function getFeedHtml(){
         </div>
     </div> 
 </div>
-`
-   })
-      
-   return feedHtml 
+`;
+  });
+
+  return feedHtml;
 }
 
- function handleReplies(tweetId){
-     
-     const targetTweetObj = tweetsData.filter(function(tweet){
-        return tweet.uuid === tweetId
-     })[0]
-    
-     document.getElementById(`reply-btn-${tweetId}`).addEventListener('click', function(){
-         
-     const replyInput = document.getElementById(`${tweetId}`)
-     
-     if(replyInput){
-     targetTweetObj.replies.push({
-            handle: `@Nabeel ✅`,
-            profilePic: `images/Nabil.jpg`,
-            tweetText: `${replyInput.value}`,
-        })   
-     }
-     
-    localStorage.setItem('targetTweetObj.replies', JSON.stringify(targetTweetObj.replies))
-    
-    let replyFromLocalStorage = JSON.parse(localStorage.getItem('targetTweetObj.replies'))
-    if (replyFromLocalStorage) {
-        targetTweetObj.replies = replyFromLocalStorage 
-    }
-    
-    render()
-    replyInput.value = ''
-    
-    })   
- }
- 
- function render(){
-    document.getElementById('feed').innerHTML = getFeedHtml()
+function handleReplies(tweetId) {
+  const targetTweetObj = tweetsData.filter(function (tweet) {
+    return tweet.uuid === tweetId;
+  })[0];
+
+  document
+    .getElementById(`reply-btn-${tweetId}`)
+    .addEventListener("click", function () {
+      const replyInput = document.getElementById(`${tweetId}`);
+
+      if (replyInput) {
+        targetTweetObj.replies.push({
+          handle: `@Nabeel ✅`,
+          profilePic: `images/Nabil.jpg`,
+          tweetText: `${replyInput.value}`,
+        });
+      }
+
+      localStorage.setItem(
+        "targetTweetObj.replies",
+        JSON.stringify(targetTweetObj.replies)
+      );
+
+      let replyFromLocalStorage = JSON.parse(
+        localStorage.getItem("targetTweetObj.replies")
+      );
+      if (replyFromLocalStorage) {
+        targetTweetObj.replies = replyFromLocalStorage;
+      }
+
+      render();
+      replyInput.value = "";
+    });
 }
 
-render()
+function render() {
+  document.getElementById("feed").innerHTML = getFeedHtml();
+}
 
+render();
 
-function closeTweet(){
-   const tweet = document.getElementById('tweet')
-   tweet.remove()
+function closeTweet() {
+  const tweet = document.getElementById("tweet");
+  tweet.remove();
 }
